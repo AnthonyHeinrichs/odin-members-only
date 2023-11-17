d2521188558455a99ecef2efab0eff84fb833202
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-11-17 13:10:49 +0100

    Add messages as a partial, connect a js script file to main index html

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+const showSignUpModal = () => {
+  console.log('Show button')
+}
+
+const signUpButton = document.getElementById('button2')
+
+signUpButton.addEventListener("click", showSignUpModal);
+
