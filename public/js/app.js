21d7740c9edc7fe2e2ffd16068e2afad2a18d28d
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-11-20 18:24:15 +0100

    Setup sign in and sign up modals

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,34 @@
-const showSignUpModal = () => {
-  console.log('Show button')
+// Display/hide sign up modal
+const signUpModal = document.getElementById("sign_up");
+
+const signUpButton = document.getElementById('sign_up_btn');
+
+signUpButton.onclick = () => {
+  signUpModal.style.display = "block";
 }
 
-const signUpButton = document.getElementById('button2')
+window.onclick = (event) => {
+  if (event.target == signUpModal) {
+    signUpModal.style.display = "none";
+  }
+}
 
-signUpButton.addEventListener("click", showSignUpModal);
+// Display/hide sign in modal
+const signInModal = document.getElementById("sign_in");
+
+const signInButton = document.getElementById('sign_in_btn');
+
+signInButton.onclick = () => {
+  signInModal.style.display = "block";
+}
+
+// Closing modals when clicked outside of the modal
+window.onclick = (event) => {
+  if (event.target == signUpModal) {
+    signUpModal.style.display = "none";
+  } else if (event.target == signInModal) {
+    signInModal.style.display = "none";
+  }
+}
+
 
