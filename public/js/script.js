b6d5dd45600a5d4290252e0cadbc6db170bde518
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-11-28 17:04:30 +0100

    Add x close button to modals and update design

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 // Display/hide sign up modal
 const signUpModal = document.getElementById("sign_up");
-
 const signUpButton = document.getElementById('sign_up_btn');
+const closeSignUp = document.getElementsByClassName("close_sign_up")[0];
 
 if (signUpButton) {
   signUpButton.onclick = () => {
     signUpModal.style.display = "block";
   }
-  
+
   window.onclick = (event) => {
     if (event.target == signUpModal) {
       signUpModal.style.display = "none";
@@ -17,15 +17,12 @@ if (signUpButton) {
 
 // Display/hide sign in modal
 const signInModal = document.getElementById("sign_in");
-
 const signInButton = document.getElementById('sign_in_btn');
+const closeSignIn = document.getElementsByClassName("close_sign_in")[0]; 
 
 if (signInButton) {
-  signInButton.onclick = () => {
-    signInModal.style.display = "block";
-  }
-  
-  // Closing modals when clicked outside of the modal
+  signInButton.onclick = () => signInModal.style.display = "block";
+
   window.onclick = (event) => {
     if (event.target == signUpModal) {
       signUpModal.style.display = "none";
@@ -34,6 +31,14 @@ if (signInButton) {
     }
   }
 }
+
+// When the user clicks on x, close the sign in modal
+closeSignIn.onclick = () => signInModal.style.display = "none";
+
+// When the user clicks on x, close the sign up modal
+closeSignUp.onclick = () => signUpModal.style.display = "none";
+
+// Checking validation for sign up form
 
 document.getElementById('sign_up_form').addEventListener('submit', async function (event) {
   event.preventDefault();
